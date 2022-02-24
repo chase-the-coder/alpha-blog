4823db916e650cff07a941c81c9f25a44e0afaf4
Chase <chase.lindsay1@gmail.com>
2022-02-24 16:50:18 -0600

    added show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
   get "about", to: "pages#about"
+  get "article/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
