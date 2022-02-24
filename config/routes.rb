79c571c31d501298e4321a737b46248f96e16c41
Chase <chase.lindsay1@gmail.com>
2022-02-24 17:28:32 -0600

    add articles indes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
-  get "about", to: "pages#about"
-  get "article/:id", to: "articles#show"
+  get "/about", to: "pages#about"
+  get "/articles", to: "articles#index"
+  get "/article/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
