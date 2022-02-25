70b8a1badbe7eed05a123085544ad61f42854108
Chase <chase.lindsay1@gmail.com>
2022-02-25 13:00:52 -0600

    added new and edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,34 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+        
+    end
+    def create
+        @article = Article.new(user_params)
+        if @article.save
+            flash[:notice] = "Article was created successfully." #success message for creating an article flash[:notice] flash[:alert]
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(user_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+
+    end
+    def user_params #only accepts these fields for the user to submit
+        params.require(:article).permit(:title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
-  get "/about", to: "pages#about"
-  get "/articles", to: "articles#index"
-  get "/article/:id", to: "articles#show"
+  get "about", to: "pages#about"
+  # get "articles", to: "articles#index"
+  # post "articles", to: "articles#create"
+  # get "articles/new", to: "articles#new"
+  # get "articles/:id", to: "articles#show"
+  resources :articles
+ 
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
